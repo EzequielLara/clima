3864ef86de41ac9170ca1923828130fe543d4229
EzequielLara <ezk2442@gmail.com>
2020-08-13 17:38:12 +0200

    Creando state para Formulario

--- src/componentes/Formulario.js
+++ src/componentes/Formulario.js
@@ -1,6 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 const Formulario = () =>{
+
+    // state del formulario
+    const [busqueda, guardarBusqueda]=useState({
+        ciudad:'',
+        pais:''
+    });
+
+    //extraer ciudad y pais
+    const {ciudad, pais} = busqueda;
+    //función que coloca los elementos en el state
+    const handleChange = e =>{
+        //actualizar el state
+        guardarBusqueda({
+            ...busqueda, 
+            [e.target.name] : e.target.value
+        });
+    }
 
     return(
         <form>
@@ -8,15 +25,26 @@ const Formulario = () =>{
                 <input 
                     type="text"
                     name="ciudad"
-                    id="ciudad"  
+                    id="ciudad"
+                    value={ciudad}
+                    onChange={handleChange}  
                 />
                 <label htmlFor="ciudad">Ciudad: </label>
             </div>
             <div className="input-field col s12">
                 <select 
                     name="pais"
-                >
-                    <opcion value="">--Seleccione un pais--</opcion>
+                    id="pais"
+                    value="pais"
+                    onChange={handleChange}>
+                    <option value="">-Seleccione un pais-</option>
+                    <option value="US">Estados Unidos</option>
+                    <option value="MX">México</option>
+                    <option value="AR">Argentina</option>
+                    <option value="CO">Colombia</option>
+                    <option value="CR">Costa Rica</option>
+                    <option value="ES">España</option>
+                    <option value="PE">Perú</option>
                 </select>
                 <label htmlFor="pais">País: </label>
             </div>
